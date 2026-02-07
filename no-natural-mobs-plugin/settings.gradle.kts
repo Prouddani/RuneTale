e3ed84c51f0fafc2b81c6226414ffaef683e8ee8
Prouddani <daniel.lintz@icloud.com>
2026-02-07 09:20:43 -0300

    one of many...
    idk i thought the title was cool

---
+++ no-natural-mobs-plugin/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "no-natural-mobs-plugin"
---
+++ no-natural-mobs-plugin/src/main/java/me/prouddani/NoNaturalMobsPlugin.java
@@ -0,0 +1,12 @@
+package me.prouddani;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class NoNaturalMobsPlugin extends JavaPlugin {
+    public NoNaturalMobsPlugin(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+}
